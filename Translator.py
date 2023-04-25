d12819a7eaa4e66dfa7c153d4af55daf77742288
Yash Jain <114145222+yash-jain-1@users.noreply.github.com>
2023-04-25 23:10:24 +0530

    Create Translator.py

---
+++ Translator.py
@@ -0,0 +1,4 @@
+from translate import Translator
+translator = Translator(to_lang = 'French')
+translation = translator.translate("Failure doesn't mean you are a failure it just means you haven't succeeded yet.")
+print(translation)
